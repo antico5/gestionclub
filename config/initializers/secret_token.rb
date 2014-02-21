8617011cab99bfa2b07124ec8ecb86f89b62c3de
Armandito <armandito@armandito-Aspire-5542.(none)>
2014-02-21 15:32:56 -0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gestionclub::Application.config.secret_key_base = 'd6860aa07d7b1afe33eaf03618aaa683f29d0f524d1ebca5af0602407ed54593757abbe84d8fc9fcdfaad092cc431dfd550b62eab082bd1091bfbd66df3ce61e'
