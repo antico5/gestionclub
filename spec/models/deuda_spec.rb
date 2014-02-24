621413a4a4112f06e9c8b584a5d969c3273c50b9
Armando Andini <armando.andini@hotmail.com>
2014-02-24 15:35:35 -0300

    Add failing test on Deuda.

--- spec/models/deuda_spec.rb
+++ spec/models/deuda_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Deuda do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "No permite duplicados con el mismo periodo y socio" do
+    socio = Socio.create!
+    periodo = Periodo.create!
+    deuda = Deuda.create! socio: socio, periodo: periodo
+
+    deuda_duplicada = Deuda.create socio: socio, periodo: periodo
+    expect(deuda_duplicada).to_not be_valid
+  end
+
 end
