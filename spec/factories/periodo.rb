160dc2d44d798aff6e0a8357b8881634ab28be4a
Armando Andini <armando.andini@hotmail.com>
2014-02-24 15:54:25 -0300

    Use factory_girl on deuda spec

---
+++ spec/factories/periodo.rb
@@ -0,0 +1,4 @@
+FactoryGirl.define do
+  factory :periodo do
+  end
+end
--- spec/models/deuda_spec.rb
+++ spec/models/deuda_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe Deuda do
 
+
   it "No permite duplicados con el mismo periodo y socio" do
-    socio = Socio.create!
-    periodo = Periodo.create!
-    deuda = Deuda.create! socio: socio, periodo: periodo
+    socio = create :socio
+    periodo = create :periodo
+    deuda = create :deuda, socio: socio, periodo: periodo
 
     deuda_duplicada = Deuda.create socio: socio, periodo: periodo
     expect(deuda_duplicada).to_not be_valid
