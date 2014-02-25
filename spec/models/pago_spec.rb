617e74446d171b5b99daecafd49e3f10c2009023
Armando Andini <armando.andini@hotmail.com>
2014-02-25 15:30:06 -0300

    Create spec for Pago.
    Tests if adding multiple payments to a debt correctly updates its state to "paid"

--- spec/models/pago_spec.rb
+++ spec/models/pago_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Pago do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Setea la deuda como pagada cuando la suma de los pagos la cubren" do
+    #deuda = create :deuda
+    deuda = Deuda.create! monto:100, pagada: false
+    Pago.create! deuda: deuda, monto: 50
+    expect(deuda.pagada).to be_false
+    Pago.create! deuda: deuda, monto: 50
+    expect(deuda.pagada).to be_true
+  end
 end
