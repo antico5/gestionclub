a175466a041a1f3b4d1a8147b355569b2b91a6b4
Armando Andini <armando.andini@hotmail.com>
2014-02-25 15:32:13 -0300

    Add the logic of updating a debt's state after creating a payment

--- app/models/pago.rb
+++ app/models/pago.rb
@@ -1,4 +1,15 @@
 class Pago < ActiveRecord::Base
+
+  after_save :actualizar_deuda
+
   belongs_to :deuda
 
+  private
+
+    def actualizar_deuda
+      if deuda.monto_pagado >= deuda.monto
+        deuda.pagada = true
+        deuda.save
+      end
+    end
 end
