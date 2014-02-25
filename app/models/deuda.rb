3c3486aee8e43b9c9e5f6cbb261fca541422a87a
Armando Andini <armando.andini@hotmail.com>
2014-02-25 15:28:17 -0300

    Added method monto_pagado to deuda

--- app/models/deuda.rb
+++ app/models/deuda.rb
@@ -1,7 +1,13 @@
 class Deuda < ActiveRecord::Base
+  
   belongs_to :socio
   belongs_to :periodo
   has_many :pagos
 
   validates_uniqueness_of :socio_id, scope: :periodo_id
+  
+  def monto_pagado
+  	return pagos.sum('monto')
+  end
+  
 end
