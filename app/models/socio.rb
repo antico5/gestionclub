fe87d540c757f2326469f13b999dea73091ff4cf
Armando Andini <armando.andini@hotmail.com>
2014-02-26 16:43:25 -0300

    Validates obligatory attributes on Socio: nombre, dni and categoria

--- app/models/socio.rb
+++ app/models/socio.rb
@@ -1,4 +1,16 @@
 class Socio < ActiveRecord::Base
+  
+  after_initialize :init
+  
   belongs_to :categoria
   has_many :deudas
+  
+  validates :categoria, presence: true
+  validates :nombre, presence: true
+  validates :dni, presence: true
+  
+  def init
+  	activo ||= true #el operador ||= setea un valor al especificado solo si era nil
+  end
+  
 end
--- spec/controllers/socios_controller_spec.rb
+++ spec/controllers/socios_controller_spec.rb
@@ -23,7 +23,7 @@ describe SociosController do
   # This should return the minimal set of attributes required to create a valid
   # Socio. As you add validations to Socio, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "nombre" => "MyString" } }
+  let(:valid_attributes) { { "nombre" => "MyString", dni: "34384378", categoria_id: FactoryGirl.create(:categoria).id } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
