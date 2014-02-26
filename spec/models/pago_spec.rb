f08a95cf00e9cba3f8c4f72873e0a84a2e24e4bd
Armando Andini <armando.andini@hotmail.com>
2014-02-26 16:46:17 -0300

    Updated and added new spec to Pago

--- spec/models/pago_spec.rb
+++ spec/models/pago_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 
 describe Pago do
+
   it "Setea la deuda como pagada cuando la suma de los pagos la cubren" do
-    #deuda = create :deuda
-    deuda = Deuda.create! monto:100, pagada: false
+    deuda = create :deuda # crea una deuda de 100 pesos
     Pago.create! deuda: deuda, monto: 50
-    expect(deuda.pagada).to be_false
     Pago.create! deuda: deuda, monto: 50
     expect(deuda.pagada).to be_true
   end
+  
+  it "Debe quedar impaga cuando se hace un pago insuficiente" do
+    deuda = create :deuda
+    Pago.create! deuda: deuda, monto: 50
+    expect(deuda.pagada).to be_false
+  end
+    
 end
