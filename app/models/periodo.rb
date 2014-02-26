1611890b18d0650aa5452b2b8de2a17c944adf33
Armando Andini <armando.andini@hotmail.com>
2014-02-26 18:23:05 -0300

    Implemented the logic of generating a debt for all active members when a new period is generated.
    After creating the period one should call periodo.generar_deuda_a_socios_activos

--- app/models/periodo.rb
+++ app/models/periodo.rb
@@ -1,4 +1,34 @@
 class Periodo < ActiveRecord::Base
+
   has_many :costo_por_categorias
   has_many :deudas
+  
+  
+  def generar_deuda_a_socios_activos
+    #obtener todos los socios activos
+    socios = Socio.where activo: true
+    
+    socios.each do |socio|
+      #obtener la categoria del socio
+      categoria = socio.categoria
+      #obtener el monto adecuado para dicha categoria
+      monto_deuda = monto_por_categoria(categoria)
+      #generar la deuda para el socio
+      if monto_deuda != 0
+        deuda = Deuda.create! periodo: self, socio: socio, monto: monto_deuda
+      end
+    end
+  end
+    
+  #devuelve el monto (float) de la cuota para la categoria pasada como parametro  
+  def monto_por_categoria(categoria)
+    #obtener la instancia de CostoPorCategoria asociada a esa categoria
+    costo_por_categoria = costo_por_categorias.where(categoria: categoria).first
+    if costo_por_categoria != nil
+      return costo_por_categoria.costo
+    else
+      return 0
+    end
+  end
+  
 end
