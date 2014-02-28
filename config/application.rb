270d1f9384e2e4c81334a979924abd417d110643
Armando Andini <armando.andini@hotmail.com>
2014-02-28 17:20:56 -0300

    Changed app locale to es-AR.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Gestionclub
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    I18n.available_locales = ['es-AR', :es, :en]
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.yml').to_s]
+    I18n.default_locale = 'es-AR'
   end
 end
