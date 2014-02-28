4dacd4fe637937a9e994d65faebabb4448638e19
Armando Andini <armando.andini@hotmail.com>
2014-02-28 16:01:10 -0300

    Permitted params for categoria and periodo.

--- app/controllers/categorias_controller.rb
+++ app/controllers/categorias_controller.rb
@@ -1,3 +1,7 @@
 class CategoriasController < InheritedResources::Base
   before_filter :authenticate_user!
+  
+  def permitted_params
+      params.permit(:categoria => [:nombre])
+    end
 end
--- app/controllers/periodos_controller.rb
+++ app/controllers/periodos_controller.rb
@@ -1,3 +1,11 @@
 class PeriodosController < InheritedResources::Base
   before_filter :authenticate_user!
+  
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def permitted_params
+      params.permit(:periodo => [:nombre, :fecha_vencimiento])
+    end
+  
 end
