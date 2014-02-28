cdfbb4ecc4e3701cbb0a0b9d9bf565679b1c29fe
Armando Andini <armando.andini@hotmail.com>
2014-02-28 15:59:55 -0300

    Updated specs for using devise auth

--- spec/requests/socios_spec.rb
+++ spec/requests/socios_spec.rb
@@ -4,6 +4,7 @@ describe "Socios" do
   describe "GET /socios" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      sign_in_as_a_valid_user
       get socios_path
       response.status.should be(200)
     end
