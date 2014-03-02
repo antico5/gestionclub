a0be250d569699fa8a2225ecc8ef9e12913a4fd0
Armando Andini <armando.andini@hotmail.com>
2014-03-02 16:52:09 -0300

    Added spec for socio.

--- spec/models/socio_spec.rb
+++ spec/models/socio_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Socio do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Calcula el saldo de su cuenta" do
+    periodo = create :periodo
+    socio = Socio.create! nombre: "Juan", dni: "23423", categoria_id: 1
+    deuda = socio.generar_deuda periodo
+    pago = Pago.create deuda: deuda, monto: 60
+    expect(socio.saldo_cuenta).to eq(40)
+  end
 end
