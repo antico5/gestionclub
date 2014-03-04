9f2da365cfb7e5887f34e7f4623783799916c83d
Armando Andini <armando.andini@hotmail.com>
2014-03-04 02:51:28 -0300

    Newly created Periodo assigns itself a set of CostoPorCategoria per each existing Categoria with 0 cost.

--- app/models/periodo.rb
+++ app/models/periodo.rb
@@ -2,12 +2,23 @@ class Periodo < ActiveRecord::Base
 
   has_many :costo_por_categorias
   has_many :deudas
-  
+  has_many :categorias, through: :costo_por_categorias
+
+  accepts_nested_attributes_for :costo_por_categorias
+
+  before_create :init
+
+  def init
+    Categoria.all.each do |cat|
+      costo_por_categorias.build categoria: cat, costo: 0
+    end
+  end
+
   def generar_deuda_a_socios_activos
     #obtener todos los socios activos
     Socio.activos.each do |socio|
       socio.generar_deuda self
     end
   end
-  
+
 end
