7142636ad602e120f79735b2f4a0f5c6a778c591
Armando Andini <armando.andini@hotmail.com>
2014-03-04 03:56:34 -0300

    Period's costs per category can be filled on the period's edit page.

--- app/controllers/periodos_controller.rb
+++ app/controllers/periodos_controller.rb
@@ -1,18 +1,18 @@
 class PeriodosController < InheritedResources::Base
   before_filter :authenticate_user!
-  
+
   def generar_deuda
     @periodo = Periodo.find params[:id]
     @periodo.generar_deuda_a_socios_activos
     flash[:notice] = "Generadas deudas a socios para el periodo '"+@periodo.nombre+"'. Total generadas: "+ Deuda.where(periodo: @periodo).count.to_s
     redirect_to periodos_path
   end
-  
+
   private
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def permitted_params
-      params.permit(:periodo => [:nombre, :fecha_vencimiento])
+      params.permit(:periodo => [:nombre, :fecha_vencimiento, :costo_por_categorias, costo_por_categorias_attributes: [:id, :categoria_id, :costo]])
     end
-  
+
 end
