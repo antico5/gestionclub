d6e6deb3a3fb683620184c447657e436b5a78722
Armando Andini <armando.andini@hotmail.com>
2014-03-04 23:20:09 -0300

    Added spec for socio: it should return its debt and payment history

--- spec/models/socio_spec.rb
+++ spec/models/socio_spec.rb
@@ -8,4 +8,13 @@ describe Socio do
     pago = Pago.create deuda: deuda, monto: 60
     expect(socio.saldo_cuenta).to eq(40)
   end
+
+  it "Devuelve el listado de su cuenta corriente" do
+    periodo = create :periodo
+    socio = Socio.create! nombre: "Juan", dni: "23423", categoria_id: 1
+    deuda = socio.generar_deuda periodo
+    pago = Pago.create deuda: deuda, monto: 60
+
+    expect(socio.listar_cuenta_corriente.size).to eq(2)
+  end
 end
