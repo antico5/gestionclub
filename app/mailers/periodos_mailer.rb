7953eb174cc54ce35dcc993498de970ab3a3246b
Armando Andini <armando.andini@hotmail.com>
2014-05-06 12:50:57 -0300

    Periodos mailer created.

--- app/mailers/periodos_mailer.rb
+++ app/mailers/periodos_mailer.rb
@@ -1,14 +1,15 @@
 class PeriodosMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "administracion@gestionclub.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.periodos_mailer.notify_liquidation.subject
   #
-  def notify_liquidation
-    @greeting = "Hi"
+  def notify_liquidation periodo, usuario
+    @periodo = periodo
+    @usuario = usuario
 
-    mail to: "to@example.org"
+    mail to: @usuario.email, subject: "Periodo liquidado: "+periodo.nombre, from: "GestionClub"
   end
 end
